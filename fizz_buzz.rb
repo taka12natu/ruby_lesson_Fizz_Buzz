bd2ff565f0748809555e3fe2950604810e25d6dd
taka12natu <hvhpdrmcmtr12@gmail.com>
2020-08-04 18:36:32 +0900

    modify

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,11 +1,16 @@
-puts "数値を入力して下さい。"
-num = gets.to_i
-if  num%15 == 0
-	puts "FizzBuzz"
-elsif num%3 == 0
-	puts "Fizz"
-elsif num%5 == 0
-	puts "Buzz"
-else 
-	puts num.to_s
+def calculate(num)
+	if  num%15 == 0
+		"FizzBuzz"
+	elsif num%3 == 0
+		"Fizz"
+	elsif num%5 == 0
+		 "Buzz"
+	else 
+		num.to_s
+	end
 end
+
+
+puts "数値を入力して下さい。"
+input = gets.to_i
+puts calculate(input)
